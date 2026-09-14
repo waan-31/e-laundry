c0365bd2ec78846f517b608c464616d98429c420
Ramadani <ramadanimadan055@gmail.com>
2026-09-14 14:49:58 +0700

    instalasi dan pengisian model & migrasi

---
+++ database/migrations/2026_09_14_072702_create_order_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')
+            ->constrained('orders')
+            ->onDelete('cascade');
+            $table->foreignId('service_id')
+            ->constrained('services')
+            ->onDelete('cascade');
+            $table->decimal('qty', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
